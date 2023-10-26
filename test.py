e2206e040c4d923c32abb1d5bb6669c1981eec69
filip k <filip2006kujawa@gmail.com>
2023-10-26 22:05:56 +0200

    :)

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ from .main import konwersjanaszesnasatkowy
 def test_checks_if_a_number_is_negative():
     assert konwersjanaszesnasatkowy(-3455) == "Podana liczba jest ujemna, podaj dodatnia liczbe"
     assert konwersjanaszesnasatkowy(0) == "0"
+
+def test_checks_if_number_is_positive():
+    assert konwersjanaszesnasatkowy(99) == "63"
+    assert konwersjanaszesnasatkowy(100) == "64"
