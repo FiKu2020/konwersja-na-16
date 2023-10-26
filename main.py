f7aeeadca26c71504569d7a6302aa86d15a073ef
filip k <filip2006kujawa@gmail.com>
2023-10-26 20:54:30 +0200

    cos dziala?????????

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-x = int(input())
 def konwersjanaszesnasatkowy(x):
     if x < 0:
-        return "podaj dodatnia"
-wygladwhexadecymalnym = ''
-while x > 0:
-    resztaDzielenia = x % 16
-if resztaDzielenia > 10:
-    wygladwhexadecymalnym = resztaDzielenia + wygladwhexadecymalnym;
-else:
-    pass
+        return ("podaj dodatnia")
+    wygladwHexadecymalnym = ''
+
+    while x > 0:
+        resztaDzielenia = x % 16
+    if resztaDzielenia > 10:
+        wygladwhexadecymalnym = resztaDzielenia + wygladwhexadecymalnym;
+    else:
+        wygladwhexadecymalnym = chr(ord('A') + resztaDzielenia - 10) + wygladwhexadecymalnym
+    return wygladwhexadecymalnym
+
+
+
+
+x = int(input("Podaj dodatnia cyfre: "))
+print(wygladwhexadecymalnym)
 
 
 
