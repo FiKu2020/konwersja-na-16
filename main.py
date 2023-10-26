0744be37e615cbc11062c64eb633ffff02d43c0b
filip k <filip2006kujawa@gmail.com>
2023-10-26 21:46:28 +0200

    cos dziala moze

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 def konwersjanaszesnasatkowy(x):
     if x < 0:
-        return ("podaj dodatnia")
-    wygladwHexadecymalnym = ''
+        return "Podaj liczbę dodatnią"
+
+    wygladwhexadecymalnym = ''
 
     while x > 0:
         resztaDzielenia = x % 16
-    if resztaDzielenia > 10:
-        wygladwhexadecymalnym = resztaDzielenia + wygladwhexadecymalnym;
-    else:
-        wygladwhexadecymalnym = chr(ord('A') + resztaDzielenia - 10) + wygladwhexadecymalnym
-    x = x // 16
+        x = x // 16
+        if resztaDzielenia < 10:
+            wygladwhexadecymalnym = str(resztaDzielenia) + wygladwhexadecymalnym
+        else:
+            wygladwhexadecymalnym = chr(ord('A') + resztaDzielenia - 10) + wygladwhexadecymalnym
 
     return wygladwhexadecymalnym
 
-x = int(input("Podaj Dodatnia Cyfre: "))
+
+x = int(input("Podaj liczbę dodatnią: "))
 ostWynik = konwersjanaszesnasatkowy(x)
-print("wyglad w systemie szesnastkowym: ", ostWynik)
-
-
-
-
+print("Wygląd w systemie szesnastkowym:", ostWynik)
