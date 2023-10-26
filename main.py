496b972e6c6e7959623eaec99f2752a7d67d0bc2
filip k <filip2006kujawa@gmail.com>
2023-10-26 19:45:30 +0200

    qwe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def konwersjanaszesnasatkowy(x):
 x = int(input("podaj dodatnia cyfre: "))
 resztaDzielenia = {}
 if x < 0:
-    print("Podaj cyfre dodatnia")
-else
+    return "podaj dodatnia"
+else:
+    pass
+    
