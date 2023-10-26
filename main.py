f34c958f28c4fe2fe2c996878668cce76af3aff8
filip k <filip2006kujawa@gmail.com>
2023-10-26 20:16:44 +0200

    cos v2?

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def konwersjanaszesnasatkowy(x):
         return "podaj dodatnia"
 wygladwhexadecymalnym = ''
 while x > 0:
-resztaDzielenia = x % 16
+    resztaDzielenia = x % 16
 if resztaDzielenia > 10:
     wygladwhexadecymalnym = resztaDzielenia + wygladwhexadecymalnym;
+else:
+    pass
 
 
 
